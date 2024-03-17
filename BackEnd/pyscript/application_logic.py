12e01faf94a915d34a6ea9a9f23010c7bcdd261d
Tommaso Bonetti <tommaso2.bonetti@mail.polimi.it>
2024-03-17 19:09:36 +0100

    Link the simulation logic to the website

--- BackEnd/pyscript/application_logic.py
+++ BackEnd/pyscript/application_logic.py
@@ -4,32 +4,72 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
-@when('click', '#btn-plot')
+from gale_shapley import run_simulation
+
+@when('click', '#run-simulation')
 def display_plot(event):
-	num_points = 100
-	gen = np.random.default_rng(seed=0)
-	x = gen.random(num_points)
-	low, high = 0, 1
-	sigma = 0.2
-	y = []
-	for i, mu in enumerate(x):
-		if i % 8 == 0:
-			sigma = 1
-		y_new = stats.truncnorm((low-mu)/sigma, (high-mu)/sigma, loc=mu, scale=sigma).rvs(1)
-		y.append(y_new)
-		if i % 12 == 0:
-			sigma = 0.1
-	y = np.array(y)
-	y = y.reshape(y.shape[0],)
+	counts = np.load('bin_counts_stage1.npy')
+	averages = np.load('bin_averages_stage1.npy')
+	cumulative_counts = np.cumsum(counts[:-1])
+	num_students = np.sum(counts)
 
-	fig, ax = plt.subplots()
-	ax.set_aspect('equal')
-	ax.plot(np.linspace(0, 1, 10000), np.linspace(0, 1, 10000), color='grey', linewidth=0.5)
-	ax.scatter(x, y, c=y-x, cmap='RdYlGn')
-	ax.set_xlabel('Overall acceptance rate')
-	ax.set_ylabel('"Personalized" acceptance likelihood')
+	averages_int = np.array([int('0x' + avg, 0) for avg in averages])
+	fractions = np.zeros((12, 12))
+	for i in range(12):
+		fractions[i] += (counts[i] / cumulative_counts) * np.hstack((np.zeros(i), np.ones(12 - i))) * averages_int[i]
+	cumulative_averages = fractions.sum(axis=0)
 
-	plot = document.getElementById('plot')
-	plot.classList.add('show')
-	plot.classList.remove('hide')
+	students_top_choice = counts[0]
+	students_top_five_choice = cumulative_counts[4]
+	students_unmatched = counts[-1]
+
+	avg_top_choice = averages_int[0]
+	avg_top_five_choice = int(cumulative_averages[4])
+	avg_unmatched = averages_int[-1]
+
+	results = document.getElementById('simulation-results')
+
+	top_choice = document.getElementById('top-choice')
+	top_five_choice = document.getElementById('top-five-choice')
+	unmatched = document.getElementById('unmatched')
+
+	top_choice.innerText = (
+		f'{100*students_top_choice/num_students:.1f}% of the students were matched to their top choice '
+		f'({students_top_choice:,} out of {num_students:,}). Their average lottery number starts with the digits '
+		f'{avg_top_choice:x}.'
+	)
+
+	top_five_choice.innerText = (
+		f'{100*students_top_five_choice/num_students:.1f}% of the students were matched to one of their top five choices '
+		f'({students_top_five_choice:,} out of {num_students:,}). Their average lottery number starts with the digits '
+		f'{avg_top_five_choice:x}.'
+	)
+
+	unmatched.innerText = (
+		f'{100*students_unmatched/num_students:.1f}% of the students were unmatched '
+		f'({students_unmatched:,} out of {num_students:,}). Their average lottery number starts with the digits '
+		f'{avg_unmatched:x}.'
+	)
+
+	fig, ax1 = plt.subplots()
+	bins = range(1, 14)
+
+	ax1.bar(bins, 100*counts/num_students, color='orange')
+	ax1.set_xticks(bins, list(range(1, 13)) + ['U'])
+	ax1.set_xlabel('Position on preference list')
+	ax1.set_ylabel('Percentage of students matched')
+
+	ax2 = ax1.twinx()
+	ax2.plot(bins, averages_int, color='steelblue', marker='x', linewidth=0.5)
+	bottom, top = ax2.get_ylim()
+	labels = [4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+	label_nums = ['0x' + str(i) + '0000000' for i in labels]
+	ticks = [int(l, 0) for l in label_nums]
+	ax2.set_yticks(ticks, labels)
+	ax2.set_ylim(bottom, top)
+	ax2.set_ylabel('Average lottery number (first digit)')
+
 	display(fig, target='plot', append=False)
+
+	results.classList.add('show')
+	results.classList.remove('hide')
