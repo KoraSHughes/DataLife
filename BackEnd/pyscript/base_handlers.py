f434c3a37a8fdde32815e896411e58283b6b420d
Tommaso Bonetti <tommaso2.bonetti@mail.polimi.it>
2024-03-20 15:40:50 +0100

    Change gradient for better accessibility

--- BackEnd/pyscript/base_handlers.py
+++ BackEnd/pyscript/base_handlers.py
@@ -1,28 +1,6 @@
+from pyscript import PyWorker, when
 
-from js import alert, console
-from pyscript import document, when
-
-@when('click', '#btn-about')
-def about(event):
-	alert('About')
-
-@when('click', '#btn-how')
-def about(event):
-	alert('How it works')
-
-@when('click', '#btn-qa')
-def about(event):
-	alert('Q&A')
-
-@when('change', '#input-district-no')
-@when('change', '#input-screen-group')
-def check_placeholder(event):
-	dropdown = event.srcElement
-	if (dropdown.value != ''):
-		dropdown.classList.remove('select-no-option')
-
-@when('click', '#search-btn')
-def submit_form(event):
-	form = document.getElementById('search-form')
-	console.log('Function call: submitForm()')
-	console.log(form)
+@when('click', '#run-simulation')
+def run_simulation(event):
+	worker = PyWorker('./application_logic.py')
+	# TODO what's going on????
