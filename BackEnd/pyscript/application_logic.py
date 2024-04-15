b88f2e9824656a9d1e33be5130056aed39db1d67
Tommaso Bonetti <tommaso2.bonetti@mail.polimi.it>
2024-04-15 11:08:03 +0200

    Update backend application logic

--- BackEnd/pyscript/application_logic.py
+++ BackEnd/pyscript/application_logic.py
@@ -1,4 +1,5 @@
 from pyscript import display, document, when
+from pyscript.js_modules import script as js
 
 import bisect
 
@@ -7,15 +8,35 @@ import matplotlib.pyplot as plt
 
 import gale_shapley as gs
 
-def load_simulation_results(student_id, student_lottery, student_list, stage=2):
-	counts = np.load(f'bin_counts_stage{stage}.npy')
-	averages = np.load(f'bin_averages_stage{stage}.npy')
-	ranges = np.load(f'bin_ranges_stage{stage}.npy')
-	matches = np.load(f'matches_stage{stage}.npy', allow_pickle=True).item()
-	return counts, averages, ranges, matches
+STUDENT_ID = 'current_user'
+
+def dict_to_list(dictionary, sort=True) -> list:
+	lst = [None] * len(dictionary.keys())
+	for i in range(len(lst)):
+		val = dictionary.get(i)
+		if len(val) == 0:
+			val = ['0000']
+		lst[i] = sorted(val) if sort else val
+	return lst
+
+def hex_to_int(numbers: list[str]) -> np.array:
+	numbers_truncated = [num[:8] if len(num) > 8 else num for num in numbers]
+	return np.array([int('0x' + num, 0) for num in numbers_truncated])
+
+def load_simulation_results(student_lottery, student_list, range_pct=0, rs=2):
+	bins = dict_to_list(np.load(f'sim/bins.npy', allow_pickle=True).item())
+	matches = np.load(f'sim/matches.npy', allow_pickle=True).item()
+
+	counts = np.array([len(bin) for bin in bins])
+	medians = [bin[len(bin)//2] for bin in bins]
+	range_mins = [min(bin) if range_pct == 0 else bin[int(len(bin) * (range_pct/100))] for bin in bins]
+	range_maxs = [max(bin) if range_pct == 0 else bin[int(-len(bin) * (range_pct/100))] for bin in bins]
+
+	print(counts)
+	return counts, medians, range_mins, range_maxs, matches
 
 	# Load the saved rankings
-	students = np.load(f'student_rankings_stage{stage}.npy', allow_pickle=True).item()  # { 'student_XXXXX': ['YYYYYY', ...], ... }
+	students = np.load(f'student_rankings_stage{rs}.npy', allow_pickle=True).item()  # { 'student_XXXXX': ['YYYYYY', ...], ... }
 	lottery = dict(np.load('student_demographics.npy', allow_pickle=True)[:, (0, 17)])  # { 'student_XXXXX': '...', ... }
 	schools = np.load('school_rankings.npy', allow_pickle=True).item()  # { 'YYYYYY': ['lottery', ...], ... }
 	capacities = np.load('school_capacities.npy', allow_pickle=True).item()
@@ -24,59 +45,29 @@ def load_simulation_results(student_id, student_lottery, student_list, stage=2):
 	for s_id in student_list:
 		bisect.insort(schools[s_id], student_lottery)
 
-	students[student_id] = student_list
-	lottery[student_id] = student_lottery
+	students[STUDENT_ID] = student_list
+	lottery[STUDENT_ID] = student_lottery
 
 	# Run the algorithm
-	return gs.run_simulation(students, lottery, schools, capacities, stage)
+	return gs.run_simulation(students, lottery, schools, capacities, rs)
 
 def display_plot(event):
-	student_id = None  # document.getElementById('student-id').value
 	student_lottery = None  # document.getElementById('student-lottery').value
 	student_list = None  # document.getElementById('student-list').value.split()
 
-	counts, averages, ranges, matches = load_simulation_results(student_id, student_lottery, student_list, stage=1)
-	counts = np.array(counts)
-	averages = np.array(averages)
-	cumulative_counts = np.cumsum(counts[:-1])
+	counts, medians, range_mins, range_maxs, matches = load_simulation_results(student_lottery, student_list, rs=1)
 	num_students = np.sum(counts)
 
-	averages_int = np.array([int('0x' + avg, 0) for avg in averages])
-	fractions = np.zeros((12, 12))
-	for i in range(12):
-		fractions[i] += (counts[i] / cumulative_counts) * np.hstack((np.zeros(i), np.ones(12 - i))) * averages_int[i]
-	cumulative_averages = fractions.sum(axis=0)
-
-	students_top_choice = counts[0]
-	students_top_five_choice = cumulative_counts[4]
 	students_unmatched = counts[-1]
+	median_unmatched = medians[-1]
 
-	avg_top_choice = averages_int[0]
-	avg_top_five_choice = int(cumulative_averages[4])
-	avg_unmatched = averages_int[-1]
+	pct_unmatched = document.getElementById('pct-unmatched')
+	num_unmatched = document.getElementById('num-unmatched')
+	med_unmatched = document.getElementById('med-unmatched')
 
-	top_choice = document.getElementById('top-choice')
-	top_five_choice = document.getElementById('top-five-choice')
-	unmatched = document.getElementById('unmatched')
-	# customized = document.getElementById('customized')
-
-	top_choice.innerText = (
-		f'{100*students_top_choice/num_students:.1f}% of the students were matched to their top choice '
-		f'({students_top_choice:,} out of {num_students:,}). Their average lottery number starts with the digits '
-		f'{avg_top_choice:x}.'
-	)
-
-	top_five_choice.innerText = (
-		f'{100*students_top_five_choice/num_students:.1f}% of the students were matched to one of their top five choices '
-		f'({students_top_five_choice:,} out of {num_students:,}). Their average lottery number starts with the digits '
-		f'{avg_top_five_choice:x}.'
-	)
-
-	unmatched.innerText = (
-		f'{100*students_unmatched/num_students:.1f}% of the students were unmatched '
-		f'({students_unmatched:,} out of {num_students:,}). Their average lottery number starts with the digits '
-		f'{avg_unmatched:x}.'
-	)
+	pct_unmatched.innerText = f'{100*students_unmatched/num_students:.1f}%'
+	num_unmatched.innerText = f'That\'s {students_unmatched:,} students out of {num_students:,}.'
+	med_unmatched.innerText = f'Their median lottery number starts with the digits {median_unmatched[:4]}.'
 
 	# school, rank = matches['student_test']
 	# customized.innerText = f'Test student matched to school {school}, ranked {rank} on their preference list.'
@@ -93,32 +84,37 @@ def display_plot(event):
 	fig.set_facecolor('#ffffff00')
 	display(fig, target='plot', append=False)
 
-@when('click', '#run-simulation')
-async def on_click(event):
-	loader = document.getElementById('simulation-loader')
-	results = document.getElementById('simulation-results')
+@when('click', '#run-simulation-py')
+def on_click(event):
+	if event.target.id != 'run-simulation':
+		print('Event ignored')
+		return
 
-	# Hide simulation results (if applicable)
-	# print('Hide results')
-	results.classList.add('hide')
-	results.classList.remove('show')
+	print(js.customData)
+	# loader = document.getElementById('simulation-loader')
+	# results = document.getElementById('simulation-results')
 
-	# Display loading animation
-	# print('Show loader')
-	loader.classList.add('show')
-	loader.classList.remove('hide')
+	# # Hide simulation results (if applicable)
+	# # print('Hide results')
+	# results.classList.add('hide')
+	# results.classList.remove('show')
+
+	# # Display loading animation
+	# # print('Show loader')
+	# loader.classList.add('show')
+	# loader.classList.remove('hide')
 
 	# Schedule the simulation (slow task) on a different thread, so the UI thread is not blocked
 	# simulation_task = asyncio.to_thread(display_plot, None)
 	# await simulation_task
 	display_plot(None)
 
-	# Hide loading animation
-	# print('Hide loader')
-	loader.classList.add('hide')
-	loader.classList.remove('show')
+	# # Hide loading animation
+	# # print('Hide loader')
+	# loader.classList.add('hide')
+	# loader.classList.remove('show')
 
-	# Display simulation results
-	# print('Show results')
-	results.classList.add('show')
-	results.classList.remove('hide')
+	# # Display simulation results
+	# # print('Show results')
+	# results.classList.add('show')
+	# results.classList.remove('hide')
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,17 +1,26 @@
 const lotteryNumbers = [
 	'3a4bdc7f-8e91-0c2a-6f5d-1b9e6a38d047',
-	'3833cfd2-93c5-b281-407b-c3b96160ea3f',
-	'9568a214-f388-402d-4145-45e8138643e',
-	'cb6e6546-cfd6-0134-d85a-5e8041f8b06c',
-	'd8b0965e-9e71-c53b-f849-953a82e164d1',
 	'0c68f153-12e5-3e7f-2234-6517592f1763',
+	'9568a214-f388-402d-4145-45e8138643e',
+	'7833cfd2-93c5-b281-407b-c3b96160ea3f',
+	'd8b0965e-9e71-c53b-f849-953a82e164d1',
 	'211cee28-3c31-bf81-4a16-6f39344b9f97',
+	'cb6e6546-cfd6-0134-d85a-5e8041f8b06c',
 	'4f6bd3ef-2929-d7c0-8215-c4065d0109b4'
 ];
 let point = 0;
 
+const schoolNames = [
+	'John F. Kennedy HS (1)',
+	'Stuyvesant HS (2)',
+	'Marie Curie Scientific HS (3)',
+	'Staten Island HS for Ferry Captains (4)'
+];
+
+let customData = false;
+
 function updateLotteryNumber() {
-	el = document.getElementById('lottery-example');
+	let el = document.getElementById('lottery-example');
 	el.innerText = lotteryNumbers[point];
 	if (point < lotteryNumbers.length - 1)
 		point++;
@@ -19,8 +28,75 @@ function updateLotteryNumber() {
 		point = 0;
 }
 
-window.onload = () => {
-	console.log('onWindowLoad')
+function displayAddButton(event) {
+	let addButton = document.getElementById('add-choice');
+	let choices = document.getElementById('choices');
+	let lastChoice = choices.children[choices.children.length - 2];
+
+	if (lastChoice.value !== '' && choices.childElementCount < 15)
+		addButton.classList.remove('hide');
+}
+
+function addSchoolChoice(event) {
+	let addButton = document.getElementById('add-choice');
+	let choices = document.getElementById('choices');
+	let numChoices = choices.childElementCount - 3;
+
+	let newChoice = document.createElement('div');
+	newChoice.id = 'choice-' + String(numChoices + 1);
+	newChoice.classList.add('btn-secondary', 'input');
+
+	let icon = document.createElement('span');
+	icon.classList.add('material-symbols-outlined');
+	icon.innerHTML = 'search';
+	icon.addEventListener('click', () => input.focus());
+	newChoice.appendChild(icon);
+
+	let input = document.createElement('input');
+	input.name = 'choice-' + String(numChoices + 1);
+	input.placeholder = 'Choice ' + String(numChoices + 1);
+	input.size = 18;
+	input.setAttribute('list', 'school-list');
+	newChoice.appendChild(input);
+
+	newChoice.addEventListener('change', displayAddButton);
+
+	choices.insertBefore(newChoice, addButton);
+	addButton.classList.add('hide');
+}
+
+window.addEventListener('load', () => {
 	updateLotteryNumber();
 	setInterval(updateLotteryNumber, 3000);
-};
+
+	const addDataBtn = document.getElementById('btn-add-data');
+	const removeDataBtn = document.getElementById('btn-remove-data');
+	addDataBtn.addEventListener('click', () => {
+		document.getElementById('choices').classList.remove('hide')
+		addDataBtn.classList.add('hide');
+		removeDataBtn.classList.remove('hide');
+		customData = true;
+	});
+	removeDataBtn.addEventListener('click', () => {
+		document.getElementById('choices').classList.add('hide')
+		removeDataBtn.classList.add('hide');
+		addDataBtn.classList.remove('hide');
+		customData = false;
+	});
+
+	document.getElementById('run-simulation').addEventListener('click', () => {
+		console.log('Click captured');
+	});
+
+	document.getElementById('add-choice').addEventListener('click', addSchoolChoice);
+	document.getElementById('choice-1').addEventListener('change', displayAddButton);
+
+	const schoolList = document.getElementById('school-list');
+	schoolNames.forEach(opt => {
+		let option = document.createElement('option');
+		option.value = opt
+		schoolList.appendChild(option);
+	});
+});
+
+export { customData };
