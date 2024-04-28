89c620ddee55204f7ba6b0e5294e8f2c0861f593
Tommaso Bonetti <tommaso2.bonetti@mail.polimi.it>
2024-04-28 12:15:26 +0200

    Add functionality to dynamically select random state

--- BackEnd/pyscript/application_logic.py
+++ BackEnd/pyscript/application_logic.py
@@ -8,9 +8,6 @@ import gale_shapley as gs
 from oneshot import oneshot, oneshot_with_input
 
 STUDENT_ID = 'current_user'
-SEL = { 1: 'random', 2: 'popularity-based' }
-RNK = { 0: 'random', 1: 'likeability-based' }
-ADM = { 1: 'open', 2:'EdOpt', 3:'screen' }
 
 def dict_to_list(dictionary, sort=True) -> list:
 	lst = [None] * len(dictionary.keys())
@@ -36,53 +33,52 @@ def load_simulation_results(has_data, student_lottery, student_gpa, student_list
 		school_info = np.load(path_gen + 'school_info.npy', allow_pickle=True).item()
 
 		student_dict = {
-			k: { 'lottery': v[1], 'selection': SEL.get(v[2]), 'ranking': RNK.get(v[3]), 'list_length': v[4], 'gpa': v[5] }
+			k: { 'lottery': v[1], 'selection': v[2], 'ranking': v[3], 'list_length': v[4], 'gpa': v[5] }
 			for k, v in student_info.items()
 		}
 		school_dict = {
-			k: { 'policy': ADM.get(v[2]), 'popularity': v[3], 'likeability': v[4] }
+			k: { 'policy': v[2], 'popularity': v[3], 'likeability': v[4] }
 			for k, v in school_info.items()
 		}
 
 		bins = dict_to_list(np.load(path_sim + 'bins.npy', allow_pickle=True).item())
 		matches = np.load(path_sim + 'matches.npy', allow_pickle=True).item()
-		seats = np.load(path_sim + 'seats.npy', allow_pickle=True).item()
+		school_outcome = np.load(path_sim + 'school_outcome.npy', allow_pickle=True).item()
 
 	# Otherwise, execute the one-shot pipeline to obtain the input, then run the simulation
 	else:
 		students, schools, student_info, school_info = \
-			oneshot_with_input(rs, student_lottery, student_list, student_gpa or -1, student_name=STUDENT_ID)
+			oneshot_with_input(rs, student_lottery, student_list, float(student_gpa or -1), student_name=STUDENT_ID, return_list=True)
 
 		student_dict = {
-			k: { 'lottery': v[1], 'selection': SEL.get(v[2]), 'ranking': RNK.get(v[3]), 'length': v[4], 'gpa': v[5] }
+			k: { 'lottery': v[1], 'selection': v[2], 'ranking': v[3], 'list_length': v[4], 'gpa': v[5] }
 			for k, v in student_info.items()
 		}
 		school_dict = {
-			k: { 'policy': ADM.get(v[2]), 'popularity': v[3], 'likeability': v[4] }
+			k: { 'policy': v[2], 'popularity': v[3], 'likeability': v[4] }
 			for k, v in school_info.items()
 		}
 
-		bins, matches, seats = gs.run_matching(students, student_info, schools, school_info)
+		bins, matches, school_outcome = gs.run_matching(students, student_info, schools, school_info)
 		bins = dict_to_list(bins)
 
-	return student_dict, school_dict, bins, matches, seats
+	return student_dict, school_dict, bins, matches, school_outcome
 
 @when('click', '#run-simulation')
 def on_click(event):
-	random_state = 1  # TODO assign dynamically
-
-	res = js.userData.getValues().to_py()
-	has_data = res[0]
-	lottery = res[1]
-	gpa = res[2]
-	preferences = res[3]
+	res = js.userData.getValues(True).to_py()
+	has_data = res.get('hasData')
+	lottery = res.get('lottery')
+	gpa = res.get('gpa')
+	preferences = res.get('preferences')
+	random_state = res.get('rs')
 
 	# Load simulation results based on the provided data
-	students, schools, bins, matches, seats = load_simulation_results(has_data, lottery, gpa, preferences, rs=random_state)
+	students, schools, bins, matches, school_outcome = load_simulation_results(has_data, lottery, gpa, preferences, rs=random_state)
 
 	window.py_students = to_js(students)
 	window.py_schools = to_js(schools)
 	window.py_bins = to_js(bins)
 	window.py_matches = to_js(matches)
-	window.py_seats = to_js(seats)
+	window.py_school_outcome = to_js(school_outcome)
 	window.dispatchEvent(js.pyDoneEvent)
